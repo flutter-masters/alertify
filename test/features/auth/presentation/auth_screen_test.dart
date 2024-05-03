cd0a4e7612afde7122e9577d3ce57771481a82e5
FabianVarela <jfabian.303@gmail.com>
2024-05-02 21:19:23 -0500

    Create widget test for auth screen

---
+++ test/features/auth/presentation/auth_screen_test.dart
@@ -0,0 +1,32 @@
+import 'package:alertify/features/auth/presentation/view/auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Auth screen', (tester) async {
+    // arrange
+    TestWidgetsFlutterBinding.ensureInitialized();
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: AuthScreen(),
+        ),
+      ),
+    );
+
+    // act
+    final image = find.byType(Image);
+    final buttons = find.byType(ElevatedButton);
+
+    final headerText = find.text('By Flutter Dev, To Flutter Dev');
+    final mainIcon = find.byIcon(Icons.arrow_back);
+
+    // assert
+    expect(image, findsOneWidget);
+    expect(buttons, findsNWidgets(2));
+
+    expect(headerText, findsOneWidget);
+    expect(mainIcon, findsNothing);
+  });
+}
